207222b93f5d0ffba47be814762287309a1647a8
Prasanna6060 <ranjitkhanal61@gmail.com>
2024-01-17 18:30:30 +0545

    creating routes for app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SignIn";
+import Signup from "./pages/Signup";
 
-const 
-App = () => {
+const App = () => {
   return (
-    <div>
-      <h1 className='bg-slate-700 text-xl text-white m-5 p-5 rounded-3xl text-center'>
-        Welcome to react</h1>
-    </div>
-  )
-}
+    <Router>
+      <div>
+        <h1 className="bg-slate-700 text-xl text-white m-5 p-5 rounded-3xl text-center">
+          Welcome to react
+        </h1>
+        <Routes>
+          <Route path='/' element= {< Home />}/>
+          <Route path='/dashboard' element= {< Dashboard />}/>
+          <Route path='/about' element= {< About />}/>
+          <Route path='/projects' element= {< Projects />}/>
+          <Route path='sign-in' element= {< SignIn />}/>
+          <Route path='sign-up' element= {< Signup />}/>
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-export default 
-App
+export default App;
