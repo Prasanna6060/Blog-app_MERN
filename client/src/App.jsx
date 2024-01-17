fc6907c19fdfd5cd1480ffcf27c68b2245da5318
Prasanna6060 <ranjitkhanal61@gmail.com>
2024-01-17 15:46:58 +0545

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const 
+App = () => {
+  return (
+    <div>
+      <h1 className='bg-slate-700 text-xl text-white m-5 p-5 rounded-3xl text-center'>
+        Welcome to react</h1>
+    </div>
+  )
+}
+
+export default 
+App
