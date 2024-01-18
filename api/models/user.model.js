5f784334a17eacf6827e5a282a184f908f1b0ebd
Prasanna6060 <ranjitkhanal61@gmail.com>
2024-01-18 12:59:17 +0545

    creating user model and schema

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: 'string',
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: 'string',
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: 'string',
+        required: true,
+    },
+}, {timestamp: true}
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
