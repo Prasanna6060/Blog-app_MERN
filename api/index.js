7efc9f0075706c20a596624c073adb5933901430
Prasanna6060 <ranjitkhanal61@gmail.com>
2024-01-18 12:42:48 +0545

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import  express  from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.Mongo)
+.then(
+    () => {
+        console.log('Connected to Mongodb');
+    }
+).catch((err) => {
+    console.log(err);
+})
 
 const app = express();
 const port = 3000;
