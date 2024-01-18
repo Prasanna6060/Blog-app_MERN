317fbc8a983c078db9d0bbc918a64ca8207e46ae
Prasanna6060 <ranjitkhanal61@gmail.com>
2024-01-18 11:29:13 +0545

    create header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,14 +5,12 @@ import About from "./pages/About";
 import Projects from "./pages/Projects";
 import SignIn from "./pages/SignIn";
 import Signup from "./pages/Signup";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <h1 className="bg-slate-700 text-xl text-white m-5 p-5 rounded-3xl text-center">
-          Welcome to react
-        </h1>
+      <Header />
         <Routes>
           <Route path='/' element= {< Home />}/>
           <Route path='/dashboard' element= {< Dashboard />}/>
@@ -21,7 +19,6 @@ const App = () => {
           <Route path='sign-in' element= {< SignIn />}/>
           <Route path='sign-up' element= {< Signup />}/>
         </Routes>
-      </div>
     </Router>
   );
 };
